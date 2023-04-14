c710269ef52643e3946a7a2eafe3643b9cdf97fd
Annamaria Angelica De Brito Nunes <aangelic@c1r11s2.42lisboa.com>
2023-04-14 14:33:51 +0100

    edit substr.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: aangelic <aangelic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 11:21:37 by aangelic          #+#    #+#             */
-/*   Updated: 2023/04/13 15:43:17 by aangelic         ###   ########.fr       */
+/*   Updated: 2023/04/14 13:26:49 by aangelic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,12 @@ static int	ft_count_digits(long n)
 	size_t	len;
 
 	len = 0;
-	if (n == 0)
-		return (1);
-	if (n < 0)
+	if (n <= 0)
 		len++;
 	while (n)
 	{
-		n /= 10;
 		len++;
+		n = n / 10;
 	}
 	return (len);
 }
@@ -32,35 +30,28 @@ static int	ft_count_digits(long n)
 char	*ft_itoa(int n)
 {
 	char	*str;
-	size_t	len;
-	size_t	i;
+	int		len;
+	long	nb;
 
 	len = ft_count_digits(n);
-	str = (char *)malloc((len + 1) * sizeof(char));
+	str = ((char *)malloc(sizeof(char) * len + 1));
 	if (!str)
 		return (NULL);
-	str[len] = '\0';
 	if (n < 0)
 	{
 		str[0] = '-';
-		i = 1;
+		nb = -nb;
 	}
-	else
-		i = 0;
-	while (len-- > i)
+	if (nb == 0)
+		str[0] = '\0';
+	str[len--] = '\0';
+	while (nb)
 	{
-		if (n < 0)
-		{
-			str[len] = '0' + n % 10 * (-1);
-			n = n / 10;
-		}
-		else
-		{
-			str[len] = '0' + n % 10;
-			n = n / 10;
-		}
-		return (str);
+		str[len] = nb % 10 + '0';
+		len--;
+		nb = nb / 10;
 	}
+	return (str);
 }
 // int	main(void)
 // {
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: aangelic <aangelic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 11:32:50 by aangelic          #+#    #+#             */
-/*   Updated: 2023/04/12 13:47:01 by aangelic         ###   ########.fr       */
+/*   Updated: 2023/04/14 14:08:13 by aangelic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,23 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	i;
-	size_t	j;
+	size_t str_len;
 	char	*sub;
 
-	i = 0;
-	j = 0;
-	sub = (char *)malloc(sizeof(char) * (len + 1));
-	if (!s || !sub)
-		return (NULL);
-	while (s[i])
+	str_len = ft_strlen(s);
+	if (len > str_len - start)
+		len = str_len - start;
+	if (start >= str_len)
 	{
-		if (i >= start && j < len)
-		{
-			sub[j] = s[i];
-			j++;
-		}
-		i++;
+		sub = malloc(sizeof(char));
+		if (!sub)
+			return (NULL);
+		sub[0] = '\0';
+		return (sub);
 	}
-	sub[j] = '\0';
+	sub = (char *)malloc((sizeof(char) * (len + 1)));
+	if (!sub)
+		return (NULL);
+	ft_strlcpy(sub, s + start, len + 1);
 	return (sub);
 }
